e981d4a0d21d46fc49bc2ccb47f50f9057aa9b1c
Nicolas Brueckner <74188620+NicolasBrueckner@users.noreply.github.com>
2025-08-27 08:54:36 +0200

    carousel wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,7 @@
-import { webcore } from 'webcoreui/integration';// @ts-checkimport { defineConfig } from 'astro/config';
+import { webcore } from 'webcoreui/integration';
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  integrations: [webcore()]
+})
