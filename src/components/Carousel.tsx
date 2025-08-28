064ee69153c82833c0facb94d73673398d870b2f
Nicolas Brueckner <74188620+NicolasBrueckner@users.noreply.github.com>
2025-08-28 12:41:27 +0200

    style changes to project layout and carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "../styles/carousel.css";
 
+
 export default function Carousel({
   images,
   visibleCount = 1,
@@ -8,8 +9,9 @@ export default function Carousel({
   images: string[];
   visibleCount?: number;
 }) {
-  const [current, setCurrent] = useState(0);
+  const iSize = "70%"
 
+  const [current, setCurrent] = useState(0);
   const maxIndex = Math.max(images.length - visibleCount, 0);
 
   const nextSlide = () =>
@@ -31,8 +33,25 @@ export default function Carousel({
           <img key={i} src={src} alt={`Slide ${i + 1}`} />
         ))}
       </div>
-      <button className="prev" onClick={prevSlide}>‹</button>
-      <button className="next" onClick={nextSlide}>›</button>
+      <button className="prev" onClick={prevSlide}><LeftIcon width={iSize} height={iSize}/></button>
+      <button className="next" onClick={nextSlide}><RightIcon width={iSize} height={iSize}/></button>
     </div>
+  );
+}
+
+
+function LeftIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="30.08 24.98 128.78 300.03" {...props}>
+  <path fill="currentColor" d="M301.179 521.701c-3.015 7.225-3.015 14.45 0 21.675l63.232 128.311c-2.409 6.03-7.529 9.946-15.36 11.748a13789.754 13789.754 0 0 0-52.395-68.223c-17.464-22.594-35.84-45.335-55.124-68.224-7.832-9.633-7.832-18.972 0-28.017 19.285-22.888 37.66-45.477 55.124-67.768a6689.84 6689.84 0 0 0 52.395-67.768c7.831 1.214 12.951 5.13 15.36 11.747l-63.232 126.52z" transform="translate(-205.555 -358.435)"/>
+</svg>
+  );
+}
+
+function RightIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="30.08 24.98 128.82 300.03" {...props}>
+  <path fill="currentColor" d="M298.918 521.701c3.015 7.225 3.015 14.45 0 21.675l-63.26 128.311c2.409 6.03 7.529 9.946 15.36 11.748a15032.33 15032.33 0 0 1 52.422-68.223c17.465-22.594 35.84-45.335 55.125-68.224 7.832-9.633 7.832-18.972 0-28.017-19.285-22.888-37.66-45.477-55.125-67.768a6971.29 6971.29 0 0 1-52.422-67.768c-7.831 1.214-12.951 5.13-15.36 11.747l63.26 126.52z" transform="translate(-205.555 -358.435)"/>
+</svg>
   );
 }
