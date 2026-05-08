8399461103ee8c1e56f7de6a88c2f52b318263f4
Nicolas Brückner <74188620+NicolasBrueckner@users.noreply.github.com>
2026-05-08 13:07:25 +0200

    updates to conform with new astro/react/vite standards

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'react-dom/client'],
+    },
+  },
 });
