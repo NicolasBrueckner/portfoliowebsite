54c30381f0d431ec6e817c954bfe4587c9d699fc
Nicolas Brückner <74188620+NicolasBrueckner@users.noreply.github.com>
2026-05-09 02:44:41 +0200

    lots of performance and style optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-// astro.config.mjs
 // @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 
-export default defineConfig({
-  integrations: [react()],
-  vite: {
-    optimizeDeps: {
-      include: ['react', 'react-dom', 'react-dom/client'],
-    },
-  },
-});
+export default defineConfig({});
